350777a9e43d1e552baea45f842de44c04f7ffa4
Tomo7125 <Tomashrdy3@gmail.com>
2023-06-10 17:40:52 +0200

    Uprava prekladu

--- src/GUI/Game.java
+++ src/GUI/Game.java
@@ -60,11 +60,11 @@ public class Game implements ActionListener {
         array33.setIcon(iconFree);
 
         int randNumber = (Math.random() <= 0.5) ? 1 : 2;   //tento kod mi da 1 alebo 2 a podla toho budem urèova nahodne kto zaèína
-        nextPlayer.setText("Na rade je : " + ((randNumber < 2) ? newGame.getPlayer1().getName() : newGame.getPlayer2().getName())); // sem budem vipisovat nahodne kto zacina a potm sa to bude po kazdom kole strieda
+        nextPlayer.setText("It's her turn : " + ((randNumber < 2) ? newGame.getPlayer1().getName() : newGame.getPlayer2().getName())); // sem budem vipisovat nahodne kto zacina a potm sa to bude po kazdom kole strieda
     }
 
-    private void nextP1(){nextPlayer.setText("Na rade je : " + newGame.getPlayer1().getName());}
-    private void nextP2(){nextPlayer.setText("Na rade je : " + newGame.getPlayer2().getName());}
+    private void nextP1(){nextPlayer.setText("It's her turn : " + newGame.getPlayer1().getName());}
+    private void nextP2(){nextPlayer.setText("It's her turn : " + newGame.getPlayer2().getName());}
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -78,7 +78,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array11)){
             if(array11.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array11.setIcon(iconX);
                     nextP2();
                 }else {array11.setIcon(iconO); nextP1();}
@@ -87,7 +87,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array12)){
             if (array12.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array12.setIcon(iconX);
                     nextP2();
                 } else {array12.setIcon(iconO);nextP1();}
@@ -96,7 +96,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array13)){
             if (array13.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array13.setIcon(iconX);
                     nextP2();
                 } else {array13.setIcon(iconO);nextP1();}
@@ -105,7 +105,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array21)){
             if (array21.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array21.setIcon(iconX);
                     nextP2();
                 } else {array21.setIcon(iconO);nextP1();}
@@ -114,7 +114,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array22)){
             if (array22.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array22.setIcon(iconX);
                     nextP2();
                 } else {array22.setIcon(iconO);nextP1();}
@@ -123,7 +123,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array23)){
             if (array23.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array23.setIcon(iconX);
                     nextP2();
                 } else {array23.setIcon(iconO);nextP1();}
@@ -132,7 +132,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array31)){
             if (array31.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array31.setIcon(iconX);
                     nextP2();
                 } else {array31.setIcon(iconO);nextP1();}
@@ -141,7 +141,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array32)){
             if (array32.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array32.setIcon(iconX);
                     nextP2();
                 } else {array32.setIcon(iconO);nextP1();}
@@ -150,7 +150,7 @@ public class Game implements ActionListener {
         }
         if (e.getSource().equals(array33)){
             if (array33.getIcon() == iconFree) {
-                if (nextPlayer.getText().equals("Na rade je : " + newGame.getPlayer1().getName())) {
+                if (nextPlayer.getText().equals("It's her turn : " + newGame.getPlayer1().getName())) {
                     array33.setIcon(iconX);
                     nextP2();
                 } else {array33.setIcon(iconO);nextP1();}
